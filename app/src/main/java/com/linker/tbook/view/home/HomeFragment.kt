d66afbfde649d42eb5332652b2ee2a0ec97bad87
jung0115 <jungim7490@gmail.com>
2023-05-03 00:16:24 +0900

    issue #01 feature: view file package 정리

---
+++ app/src/main/java/com/linker/tbook/view/home/HomeFragment.kt
@@ -0,0 +1,19 @@
+package com.linker.tbook.view.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.linker.tbook.R
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+}
