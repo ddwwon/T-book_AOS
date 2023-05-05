9e2dbeb25c0d7a381cf4a25bff37ac767e102885
jung0115 <jungim7490@gmail.com>
2023-05-05 11:50:02 +0900

    issue #01 feature: 회원가입 레이아웃 구현

---
+++ app/src/main/java/com/linker/tbook/view/sign_up/SignUpActivity.kt
@@ -0,0 +1,13 @@
+package com.linker.tbook.view.sign_up
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.linker.tbook.R
+
+// 회원가입
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
